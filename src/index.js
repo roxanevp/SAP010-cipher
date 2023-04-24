f897c698dea7d6752fc5aefe8034e1f85533e1f1
Roxane Principe <roxane.principe@gmail.com>
2023-04-24 16:57:46 -0300

    Criado visual inicial do projeto.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+const closeButton = document.querySelector("#close");
+closeButton.onclick = function() {
+    const frame = document.querySelector("#frame");
+    frame.classList.add("hide");
+}
