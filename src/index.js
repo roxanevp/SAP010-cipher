d06b6638e509271d1ec3e790a96f6e386c9aef1a
Roxane Principe <roxane.principe@gmail.com>
2023-04-26 09:48:25 -0300

    Função para o resultado da textarea aparecer no innerHtml.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 const closeButton = document.querySelector("#close");
-closeButton.onclick = function() {
+closeButton.onclick = function(){
     const frame = document.querySelector("#frame");
     frame.classList.add("hide");
+}
+
+
+const message = document.querySelector('#message');
+message.addEventListener('input', messageChange);
+
+function messageChange(event){
+    console.log(event.target.value)
+
+    const value = event.target.value
+    const result = document.querySelector("#result")
+    result.innerHTML = value
 }
