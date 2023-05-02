bfa0c20905892b62143e440d1a023553225d87f1
Roxane Principe <roxane.principe@gmail.com>
2023-05-02 17:22:08 -0300

    Adicionado função para criptografar o texto, mudado resultado para  upperCase e função offSet para o deslocamento dos números.

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,29 @@
+const encrypt = (value, offSet) => {
+
+  const upperCaseValue = value.toUpperCase()
+  let encrypted = ""
+
+  for (let i = 0; i < upperCaseValue.length; i++) { 
+      
+      const charCode = upperCaseValue.charCodeAt(i)
+      const formulaCipher = ((charCode - 64 + offSet) % 26) + 64
+      const newChar = String.fromCharCode(formulaCipher)
+      encrypted += newChar
+  }
+
+  return encrypted
+};
+
+const decrypt = (value) => {
+
+  const upperCaseValueDe = value.toUpperCase()
+
+
+};
+
 const cipher = {
-  // ...
+  encrypt,
+  decrypt,
 };
 
 export default cipher;
