6e7616a77fd05f967d87563281e81f370c48c982
Roxane Principe <roxane.principe@gmail.com>
2023-05-02 17:38:17 -0300

    Adicionado funções criptografar e descriptografar.

--- src/cipher.js
+++ src/cipher.js
@@ -14,10 +14,21 @@ const encrypt = (value, offSet) => {
   return encrypted
 };
 
-const decrypt = (value) => {
+const decrypt = (value, offSet) => {
 
-  const upperCaseValueDe = value.toUpperCase()
+  const upperCaseValue = value.toUpperCase()
+  let encrypted = ""
 
+  for (let i = 0; i < upperCaseValue.length; i++) { 
+      
+      const charCode = upperCaseValue.charCodeAt(i)
+      const formulaCipher = ((charCode - 64 - offSet) % 26) + 64
+      const newChar = String.fromCharCode(formulaCipher)
+      encrypted += newChar
+  }
+
+  return encrypted
+  
 
 };
 
